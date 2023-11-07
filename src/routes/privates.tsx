7ea00bd6f2277c03217225edd1d90ddadb88471f
Einstein <eldson.caldasw@gmail.com>
2023-11-06 23:58:28 -0300

    private

--- src/routes/privates.tsx
+++ src/routes/privates.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react"
 import { Navigate, Outlet } from "react-router-dom"
 
 
 export const Privates = () => {
-    const [hasPermission, setHasPermission] = useState(false)
-    setHasPermission(false)
-    
+    const hasPermission = false
+
     if(!hasPermission) {
         return <Navigate to="/dashboard/sign-in"/>
     }
