35d9c6ef10e987a396af54956e556ef6f894ecd6
Einstein <eldson.caldasw@gmail.com>
2023-11-10 10:56:26 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-    // baseURL: "https://anny-back.vercel.app/",
-    baseURL: "http://localhost:3001/",
+    baseURL: "https://anny-back-a40df124d19f.herokuapp.com/",
+    // baseURL: "http://localhost:3002/",
     headers: {
         "Content-Type": "application/json",
     }
