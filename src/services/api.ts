fe2aee59ba3debdedeea3ebea9b6ee881c78e546
Einstein <eldson.caldasw@gmail.com>
2024-01-10 12:49:57 -0300

    credentials

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,5 +6,6 @@ export const api = axios.create({
     // baseURL: "http://localhost:3002/",
     headers: {
         "Content-Type": "application/json",
-    }
+    },
+    withCredentials: true,
 })
