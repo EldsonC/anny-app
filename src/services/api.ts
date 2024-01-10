51a790f113e4274cbdd56abd1a63c3e9704e1c10
Einstein <eldson.caldasw@gmail.com>
2024-01-10 12:55:44 -0300

    headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,8 @@ export const api = axios.create({
     baseURL: "https://anny-api.vercel.app/",
     // baseURL: "http://192.168.43.59:3001/",
     // baseURL: "http://localhost:3002/",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    withCredentials: true,
+    // headers: {
+    //     "Content-Type": "application/json",
+    // },
+    // withCredentials: true,
 })
